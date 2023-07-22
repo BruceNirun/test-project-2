167ee6b7119ef9c0fd7f79415e212b2c8c18117c
BruceNirun19 <nirun.h@planetcomm.com>
2023-07-22 09:07:39 +0700

    ^^

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,36 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MyPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function index() 
-    {
-        $blogs = [
-            [
-                'title' => 'Title one',
-                'body' => 'this is a body text',
-                'status' => 1
-            ],
-            [
-                'title' => 'Title two',
-                'body' => 'this is a body text',
-                'status' => 0
-            ],
-            [
-                'title' => 'Title three',
-                'body' => 'this is a body text',
-                'status' => 1
-            ],
-            [
-                'title' => 'Title four',
-                'body' => 'this is a body text',
-                'status' => 1
-            ]
-        ];
-        return view('home' , compact('blogs'));
-    }
 
-    
+    public function index()
+    {
+        
+    } 
 }
